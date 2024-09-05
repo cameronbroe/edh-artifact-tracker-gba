be523f8dd05782f6a6cff6a7114b05f87a0f2dd9
Cameron Roe <cameron@cameronbroe.com>
2024-09-05 01:06:43 -0500

    feat: show untapped count

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@
 #define ICON_X -62
 #define TOTAL_X -30
 #define TAPPED_X 0
+#define UNTAPPED_X 30
 
 static int counter = 0;
 
@@ -33,10 +34,15 @@ static int food_tapped_count = 999;
 static int treasure_tapped_count = 999;
 static int clue_tapped_count = 999;
 
+int calculate_untapped_count(int tapped, int total) {
+    return total - tapped;
+}
+
 void update_food_counters(
     bn::sprite_text_generator& text_generator, 
     bn::vector<bn::sprite_ptr, 3>& count_sprites,
-    bn::vector<bn::sprite_ptr, 3>& tapped_sprites
+    bn::vector<bn::sprite_ptr, 3>& tapped_sprites,
+    bn::vector<bn::sprite_ptr, 3>& untapped_sprites
 ) {
     bn::string<3> food_count_string;
     bn::ostringstream food_count_string_stream(food_count_string);
@@ -51,12 +57,20 @@ void update_food_counters(
 
     tapped_sprites.clear();
     text_generator.generate(TAPPED_X, FOOD_Y, food_tapped_string, tapped_sprites);
+
+    bn::string<3> food_untapped_string;
+    bn::ostringstream food_untapped_string_stream(food_untapped_string);
+    food_untapped_string_stream.append(calculate_untapped_count(food_tapped_count, food_count));
+
+    untapped_sprites.clear();
+    text_generator.generate(UNTAPPED_X, FOOD_Y, food_untapped_string, untapped_sprites);
 }
 
 void update_treasure_counters(
     bn::sprite_text_generator& text_generator, 
     bn::vector<bn::sprite_ptr, 3>& count_sprites,
-    bn::vector<bn::sprite_ptr, 3>& tapped_sprites
+    bn::vector<bn::sprite_ptr, 3>& tapped_sprites,
+    bn::vector<bn::sprite_ptr, 3>& untapped_sprites
 ) {
     bn::string<3> treasure_count_string;
     bn::ostringstream treasure_count_string_stream(treasure_count_string);
@@ -71,12 +85,20 @@ void update_treasure_counters(
 
     tapped_sprites.clear();
     text_generator.generate(TAPPED_X, TREASURE_Y, treasure_tapped_string, tapped_sprites);
+
+    bn::string<3> treasure_untapped_string;
+    bn::ostringstream treasure_untapped_string_stream(treasure_untapped_string);
+    treasure_untapped_string_stream.append(calculate_untapped_count(treasure_tapped_count, treasure_count));
+
+    untapped_sprites.clear();
+    text_generator.generate(UNTAPPED_X, TREASURE_Y, treasure_untapped_string, untapped_sprites);
 }
 
 void update_clue_counters(
     bn::sprite_text_generator& text_generator, 
     bn::vector<bn::sprite_ptr, 3>& count_sprites,
-    bn::vector<bn::sprite_ptr, 3>& tapped_sprites
+    bn::vector<bn::sprite_ptr, 3>& tapped_sprites,
+    bn::vector<bn::sprite_ptr, 3>& untapped_sprites
 ) {
     bn::string<3> clue_count_string;
     bn::ostringstream clue_count_string_stream(clue_count_string);
@@ -91,6 +113,13 @@ void update_clue_counters(
 
     tapped_sprites.clear();
     text_generator.generate(TAPPED_X, CLUE_Y, clue_tapped_string, tapped_sprites);
+
+    bn::string<3> clue_untapped_string;
+    bn::ostringstream clue_untapped_string_stream(clue_untapped_string);
+    clue_untapped_string_stream.append(calculate_untapped_count(clue_tapped_count, clue_count));
+
+    untapped_sprites.clear();
+    text_generator.generate(UNTAPPED_X, CLUE_Y, clue_untapped_string, untapped_sprites);
 }
 
 int main()
@@ -107,6 +136,10 @@ int main()
     bn::vector<bn::sprite_ptr, 3> food_tapped_sprites;
     bn::vector<bn::sprite_ptr, 3> treasure_tapped_sprites;
     bn::vector<bn::sprite_ptr, 3> clue_tapped_sprites;
+
+    bn::vector<bn::sprite_ptr, 3> food_untapped_sprites;
+    bn::vector<bn::sprite_ptr, 3> treasure_untapped_sprites;
+    bn::vector<bn::sprite_ptr, 3> clue_untapped_sprites;
 
     bn::regular_bg_ptr counter_bg = bn::regular_bg_items::counter_bg.create_bg(0, 0);
     counter_bg.set_visible(true);
@@ -129,9 +162,9 @@ int main()
         }
 
 
-        update_food_counters(counter_text_generator, food_total_sprites, food_tapped_sprites);
-        update_treasure_counters(counter_text_generator, treasure_total_sprites, treasure_tapped_sprites);
-        update_clue_counters(counter_text_generator, clue_total_sprites, clue_tapped_sprites);
+        update_food_counters(counter_text_generator, food_total_sprites, food_tapped_sprites, food_untapped_sprites);
+        update_treasure_counters(counter_text_generator, treasure_total_sprites, treasure_tapped_sprites, treasure_untapped_sprites);
+        update_clue_counters(counter_text_generator, clue_total_sprites, clue_tapped_sprites, clue_untapped_sprites);
         bn::core::update();
     }
 }
